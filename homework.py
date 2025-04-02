e2afbb717553443dabfa9176a6944a59657a40fa
SaniyaRosejoy <saniyarose003@gmail.com>
2025-04-02 07:16:31 +0000

    hm

--- homework.py
+++ homework.py
@@ -1,6 +1,5 @@
-capital = input("Enter your city:")
+capital = input("What is your capital city of Japan?\n")
 if (capital == "tokyo"):
     print("you are correct")
 else :
     print("you are not correct")
-    
