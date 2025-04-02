5189664bf354af592d1b7aaac1e551965e3e4969
SaniyaRosejoy <saniyarose003@gmail.com>
2025-04-02 08:54:44 +0000

    sumnumber

---
+++ sumnumber.py
@@ -0,0 +1,11 @@
+total = 0 
+while True:
+    try:
+        number = int(input("Enter a number  (enter 0 to exit)\n"))
+        if number == 0:
+            break 
+        else:
+            total += number
+    except ValueError:
+        print("Invalid.please enter only numbers")
+print(f"The total is : {total}")
