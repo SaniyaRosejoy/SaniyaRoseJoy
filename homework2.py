8ee9a20e7513a857b71152d9c220f10646300226
SaniyaRosejoy <saniyarose003@gmail.com>
2025-04-03 06:39:13 +0000

    hw2

---
+++ homework2.py
@@ -0,0 +1,13 @@
+correct_city = "Tokyo"
+attempt = 0
+max_attempt = 5
+input_city = ""
+
+while True:
+     capital = input("What is your capital city of Japan?\n")
+     if (capital == "tokyo"):
+      print("you are correct")
+      break
+     else :
+      print("you are not correct")
+      
