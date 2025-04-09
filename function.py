1d9264275d12d6a1a9063948e6ba46b6414816ae
SaniyaRosejoy <saniyarose003@gmail.com>
2025-04-09 10:07:53 +0000

    function

---
+++ function.py
@@ -0,0 +1,8 @@
+def display_full_name(first_name,last_name):
+    print(first_name,last_name)
+
+first_name = "Saniya Rose"
+last_name = "Joy"
+
+display_full_name(first_name, last_name)
+display_full_name("Sanika Rose", "Joy")
